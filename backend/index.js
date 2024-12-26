39245ada707a8ca6a8675e83c0b8c763e3fb21c4
Pratikdotcom <shindepratik624@gmail.com>
2024-12-26 17:21:18 +0530

    Adding packages to backend

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,9 @@
 const express = require('express')
+const cors = require('cors')
+const jwt = require('jsonwebtoken')
+const bodyParser = require('body-parser')
 const mainRouter = require('.routes/index')
+app.use(cors());
 const app = express();
 
 app.use('/api/v1', mainRouter)
