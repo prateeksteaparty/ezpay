a29b0cfe05746b9edcfb3dfef06f62f1ba5661c4
Pratikdotcom <shindepratik624@gmail.com>
2024-12-26 12:27:44 +0530

    Routes Created

---
+++ backend/database.js
@@ -0,0 +1,38 @@
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb+srv://pratik2271:pratik%401624@pratikproject.vglse6e.mongodb.net/');
+
+const userSchema = new mongoose.Schema({
+    username : {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true,
+        lowercase: true,
+        minLength: 3,
+        maxLength: 30
+    },
+    password : {
+        type: String,
+        required: true,
+        minLength: 8
+    },
+    firstName : {
+        type: String,
+        required :true,
+        trim: true,
+        maxLength: 50
+    },
+    lastName : {
+        type: String,
+        required : true,
+        trim: true,
+        maxLength : 50
+    }
+})
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = {
+    User
+}
