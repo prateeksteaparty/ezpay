bbda3b0e7e87cf8dc005955de60e537e51eab907
Pratikdotcom <shindepratik624@gmail.com>
2025-01-01 13:22:20 +0530

    Frontend Done

---
+++ frontend/ezpay/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    './src/**/*.{html,js,jsx,ts,tsx}', // Adjust paths as needed
+    './public/index.html', // Include HTML files
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
